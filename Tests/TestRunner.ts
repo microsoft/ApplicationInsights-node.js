d35e641cacd8ee20d1470a84e991dd8c03407c67
Scott Southwood <scsouthw@microsoft.com>
2014-08-11 20:18:13 -0700

    Add unit test infrastructure

---
+++ Tests/TestRunner.ts
@@ -0,0 +1,15 @@
+﻿class TestRunner {
+
+    //todo: find a good test package for node
+    public static test(name: string, action: () => void) {
+        console.log("executing test: " + name);
+        try {
+            var result = action();
+            console.log(" - success: " + result);
+        } catch (exception) {
+            console.log(" - failure: " + (<Error>exception).message);
+        }
+    }
+}
+
+module.exports = TestRunner;
--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 ﻿var http = require("http");
 var url = require("url");
 var port = process.env.port || 1337;
-var appInsights = require("applicationInsights");
 
+// load and configure application insights
+var appInsights = require("applicationInsights");
 appInsights.filter("favicon");
+
+// run tests
+var unitTests = require('./Tests/UnitTests');
+unitTests.run();
+
+// start browser and listen for requests
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
