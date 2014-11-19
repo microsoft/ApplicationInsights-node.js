e137b3e74998d19c7badd5ac9fcf77a684f50508
Scott Southwood <scsouthw@microsoft.com>
2014-11-19 13:07:54 -0800

    Add example usage

---
+++ ExampleUsage.ts
@@ -0,0 +1,29 @@
+﻿/*
+ * To run this example:
+ *  1) set APPINSIGHTS_INSTRUMENTATION_KEY=<insert_your_instrumentation_key_here>
+ *  2) npm install cookies
+ *  3) npm install node-uuid
+ *  4) node ExampleUsage.js
+ */
+
+import http = require('http');
+var port = process.env.port || 1337
+
+// before creating the server, load app-insights so we can wrap the server prototype to track requests
+var aiModule = require("applicationInsights");
+var appInsights = new aiModule.applicationInsights(/* configuration can be passed here, example: { instrumentationKey: <guid> } */);
+appInsights.filter("favicon"); // this will ignore requests for favicon
+
+// example telemetry
+appInsights.trackTrace("trace");
+appInsights.trackEvent("eventName", { custom: "properties" });
+appInsights.trackException(new Error("errorMsg"), "handledHere");
+appInsights.trackMetric("metricName", 42);
+
+// track server startup
+appInsights.startTrackEvent("server start");
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+    appInsights.stopTrackEvent("server start");
+}).listen(port);
