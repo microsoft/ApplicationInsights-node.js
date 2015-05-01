59ba9d15fb05106c8f32522f5c11df79f7dd791b
Scott Southwood <scsouthw@microsoft.com>
2015-05-01 00:18:44 -0700

    Add Logging tests

--- Library/Logging.ts
+++ Library/Logging.ts
@@ -1,18 +1,17 @@
 class Logging {
-    public static _isInternalCallActive = false;
     public static enableDebug = false;
     public static disableWarnings = false;
 
     private static TAG = "applicationInsights:";
 
     public static info(message?: any, ...optionalParams: any[]) {
-        if(Logging.enableDebug && !Logging._isInternalCallActive) {
+        if(Logging.enableDebug) {
             console.info(Logging.TAG + message, optionalParams);
         }
     }
 
     public static warn(message?: any, ...optionalParams: any[]) {
-        if(!Logging.disableWarnings && !Logging._isInternalCallActive) {
+        if(!Logging.disableWarnings) {
             console.warn(Logging.TAG + message, optionalParams);
         }
     }
