a3e7ca00cf89927a5c88057b71fb3aa11a980ff2
Luke Kim <lukim@microsoft.com>
2016-03-23 16:47:48 -0700

    Fill required StackFrame fields 'method'  and 'filename' when empty with default values.

---
+++ Tests/AutoCollection/Exceptions.tests.ts
@@ -0,0 +1,44 @@
+///<reference path="..\..\Declarations\node\node.d.ts" />
+///<reference path="..\..\Declarations\mocha\mocha.d.ts" />
+///<reference path="..\..\Declarations\sinon\sinon.d.ts" />
+
+import assert = require("assert");
+import sinon = require("sinon");
+
+import AutoCollectionExceptions = require("../../AutoCollection/Exceptions");
+
+describe("AutoCollection/Exceptions", () => {
+    describe("#getExceptionData()", () => {
+        var simpleError;
+        
+        beforeEach(() => {
+            try {
+                throw Error("simple error");
+            } catch(e) {
+                simpleError = e;
+            }
+        });
+            
+        it("fills empty 'method' with '<no_method>'", () => {
+            simpleError.stack = "  at \t (/path/file.js:12:34)\n" + simpleError.stack;
+            
+            var exceptionData = AutoCollectionExceptions.getExceptionData(simpleError, false);
+            
+            var actual = exceptionData.baseData.exceptions[0].parsedStack[0].method;
+            var expected = "<no_method>";
+            
+            assert.deepEqual(actual, expected);
+        });
+        
+        it("fills empty 'method' with '<no_filename>'", () => {
+            simpleError.stack = "  at Context.<anonymous> (\t:12:34)\n" + simpleError.stack;
+            
+            var exceptionData = AutoCollectionExceptions.getExceptionData(simpleError, false);
+            
+            var actual = exceptionData.baseData.exceptions[0].parsedStack[0].fileName;
+            var expected = "<no_filename>";
+            
+            assert.deepEqual(actual, expected);
+        });
+	});
+});
