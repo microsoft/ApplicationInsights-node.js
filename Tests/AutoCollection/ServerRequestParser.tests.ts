529ba1eff6d9983ddedc342bd0f96b42898a51d5
Louisa Godmer <lgodmer@microsoft.com>
2016-12-09 10:55:22 -0800

    Fix bug that causes custom context tag settings to be overwritten (#146)
    * - Fix bug that causes custom context tag settings to be overwritten
    - Fix a few windows build errors
    
    * fix bug where headers are set on the response object in AI sdk after the response is sent
    
    * pr feedback

--- Tests/AutoCollection/ServerRequestParser.tests.ts
+++ Tests/AutoCollection/ServerRequestParser.tests.ts
@@ -95,4 +95,36 @@ describe("AutoCollection/ServerRequestParser", () => {
             assert.equal(requestData.baseData.url, "http://bing.com/");
         });
     });
+
+    describe("#getRequestTags()", () => {
+
+        var request = {
+            method: "GET",
+            url: "/search?q=test",
+            connection: {
+                encrypted: false
+            },
+            headers: {
+                host: "bing.com",
+                "x-forwarded-for": "123.123.123.123",
+                "cookie": "ai_user=cookieUser;ai_session=cookieSession"
+            }
+        }
+
+        it("should not override context tags if they are already set", () => {
+            var helper = new ServerRequestParser(<any>request);
+
+            var originalTags: {[key: string]:string} = {
+                [(<any>ServerRequestParser).keys.locationIp]: 'originalIp',
+                [(<any>ServerRequestParser).keys.userId]: 'originalUserId',
+                [(<any>ServerRequestParser).keys.userAgent]: 'originalUserAgent',
+                [(<any>ServerRequestParser).keys.operationName]: 'originalOperationName'
+            };
+            var newTags = helper.getRequestTags(originalTags);
+            assert.equal(newTags[(<any>ServerRequestParser).keys.locationIp], 'originalIp');
+            assert.equal(newTags[(<any>ServerRequestParser).keys.userId], 'originalUserId');
+            assert.equal(newTags[(<any>ServerRequestParser).keys.userAgent], 'originalUserAgent');
+            assert.equal(newTags[(<any>ServerRequestParser).keys.operationName], 'originalOperationName');
+        });
+    });
 });
