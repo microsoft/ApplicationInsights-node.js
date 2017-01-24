fcbd534ca58794b0054f75ba79cf97ca502a2262
Kamil Szostak <kszostak@microsoft.com>
2017-01-24 12:50:46 -0800

    Allow to disable console logging

--- applicationinsights.ts
+++ applicationinsights.ts
@@ -174,8 +174,18 @@ class ApplicationInsights {
      * Enables verbose debug logging
      * @returns {ApplicationInsights} this class
      */
-    public static enableVerboseLogging() {
+    public static enableVerboseLogging(enableWarningLogging = true) {
         Logging.enableDebug = true;
+        Logging.disableWarnings = !enableWarningLogging;
+        return ApplicationInsights;
+    }
+
+    /**
+     * Disables verbose debug and warning logging
+     */
+    public static disableConsoleLogging() {
+        Logging.enableDebug = false;
+        Logging.disableWarnings = true;
         return ApplicationInsights;
     }
 
