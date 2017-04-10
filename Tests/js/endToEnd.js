ddf2988f46c121c37c4c6ff571bf62bf455d1145
Josh Gavant <josh.gavant@outlook.com>
2017-04-10 13:40:56 -0700

    fixup! update package install and build scripts

--- Tests/js/endToEnd.js
+++ Tests/js/endToEnd.js
@@ -2,7 +2,7 @@ const assert = require('assert');
 
 describe('module', function () {
     describe('#require', function () {
-        it('loads the applicationinsights module', (done) => {
+        it('loads the applicationinsights module', function (done) {
             // TODO(joshgav): do not use `assert`
             assert.doesNotThrow(require('../..'));
             done();
