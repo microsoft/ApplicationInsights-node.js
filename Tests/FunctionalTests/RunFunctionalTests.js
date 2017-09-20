a7b3b3697a5623a75d5fbce3694dd046cd661c2e
Osvaldo Rosado <osrosado@microsoft.com>
2017-09-20 10:06:48 -0700

    Change docker service ports

--- Tests/FunctionalTests/RunFunctionalTests.js
+++ Tests/FunctionalTests/RunFunctionalTests.js
@@ -64,9 +64,9 @@ function runAsync(cmd, workingDir) {
 function startDocker() {
     const tasks =  [
         run("docker run -d -p 27017:27017 --name ainjsmongo mongo"),
-        run("docker run -e MYSQL_ROOT_PASSWORD=dummypw -e MYSQL_DATABASE=testdb -d -p 3306:3306 --name ainjsmysql mysql"),
-        run("docker run -d -p 6379:6379 --name ainjsredis redis:alpine"),
-        run("docker run -d -p 5432:5432 --name ainjspostgres postgres:alpine")
+        run("docker run -e MYSQL_ROOT_PASSWORD=dummypw -e MYSQL_DATABASE=testdb -d -p 33060:3306 --name ainjsmysql mysql"),
+        run("docker run -d -p 63790:6379 --name ainjsredis redis:alpine"),
+        run("docker run -d -p 54320:5432 --name ainjspostgres postgres:alpine")
     ];
 
     for(let i = 0; i < tasks.length; i++) {
