7d4bb71d0a8eb32539e18588ad47a40315efafa3
Mark Wolff <marwolff@microsoft.com>
2018-12-18 16:16:56 -0800

    remove zonejs, add cls-hooked

--- AutoCollection/CorrelationContextManager.ts
+++ AutoCollection/CorrelationContextManager.ts
@@ -3,6 +3,8 @@ import Util = require("../Library/Util");
 import Logging = require("../Library/Logging");
 
 import * as DiagChannel from "./diagnostic-channel/initialization";
+import { createNamespace } from "cls-hooked";
+import HttpRequestParser = require("./HttpRequestParser");
 
 export interface CustomProperties {
     /**
@@ -39,6 +41,7 @@ export interface CorrelationContext {
 export class CorrelationContextManager {
     private static enabled: boolean = false;
     private static hasEverEnabled: boolean = false;
+    private static session = createNamespace('session');
 
     /**
      *  Provides the current Context.
@@ -49,7 +52,12 @@ export class CorrelationContextManager {
         if (!CorrelationContextManager.enabled) {
             return null;
         }
-        return Zone.current.get("context");
+        const context = CorrelationContextManager.session.get('context');
+
+        if (context === undefined) { // cast undefined to null
+            return null;
+        }
+        return context;
     }
 
     /**
@@ -77,15 +85,11 @@ export class CorrelationContextManager {
      *  All logical children of the execution path that entered this Context
      *  will receive this Context object on calls to GetCurrentContext.
      */
-    public static runWithContext(context: CorrelationContext, fn: ()=>any) {
+    public static runWithContext(context: CorrelationContext, fn: ()=>any): any {
         if (CorrelationContextManager.enabled) {
-            var newZone = Zone.current.fork({
-                name: "AI-" + ((context && context.operation.parentId) || "Unknown"),
-                properties: {context: context}
-            });
-            newZone.run(fn);
+            return CorrelationContextManager.session.bind(fn, {context: context})();
         } else {
-            fn();
+            return fn();
         }
     }
 
@@ -98,7 +102,7 @@ export class CorrelationContextManager {
      *  the call to wrapCallback.  */
     public static wrapCallback<T extends Function>(fn: T): T {
         if (CorrelationContextManager.enabled) {
-            return Zone.current.wrap(fn, "User-wrapped method");
+            return CorrelationContextManager.session.bind(fn);
         }
         return fn;
     }
@@ -120,26 +124,19 @@ export class CorrelationContextManager {
         if (!CorrelationContextManager.hasEverEnabled) {
             this.hasEverEnabled = true;
 
-            // Load in Zone.js
             try {
-                // Require zone if we can't detect its presence - guarded because of issue #346
-                // Note that usually multiple requires of zone.js does not error - but we see reports of it happening
-                // in the Azure Functions environment.
-                // This indicates that the file is being included multiple times in the same global scope,
-                // averting require's cache somehow.
-                if (typeof Zone === "undefined") {
-                    require("zone.js");
+                if (typeof createNamespace === "undefined") {
+                    require('cls-hooked');
                 }
             } catch (e) {
                 // Zone was already loaded even though we couldn't find its global variable
-                Logging.warn("Failed to require zone.js");
+                Logging.warn("Failed to require cls-hooked");
             }
 
             DiagChannel.registerContextPreservation((cb) => {
-                return Zone.current.wrap(cb, "AI-ContextPreservation");
+                return CorrelationContextManager.session.bind(cb, "AI-ContextPreservation");
+                // return Zone.current.wrap(cb, "AI-ContextPreservation");
             });
-            this.patchError();
-            this.patchTimers(["setTimeout", "setInterval"]);
         }
 
         this.enabled = true;
@@ -160,137 +157,6 @@ export class CorrelationContextManager {
         // Unit tests warn of errors < 3.3 from timer patching. All versions before 4 were 0.x
         var nodeVer = process.versions.node.split(".");
         return parseInt(nodeVer[0]) > 3 || (parseInt(nodeVer[0]) > 2 && parseInt(nodeVer[1]) > 2);
-    }
-
-    // Zone.js breaks concatenation of timer return values.
-    // This fixes that.
-    private static patchTimers(methodNames: string[]) {
-        methodNames.forEach(methodName => {
-            var orig = (<any>global)[methodName];
-            (<any>global)[methodName] = function() {
-                var ret = orig.apply(this, arguments);
-                ret.toString = function(){
-                    if (this.data && typeof this.data.handleId !== 'undefined') {
-                        return this.data.handleId.toString();
-                    } else {
-                        return Object.prototype.toString.call(this);
-                    }
-                }
-                return ret;
-            };
-        });
-    }
-
-    // Zone.js breaks deepEqual on error objects (by making internal properties enumerable).
-    // This fixes that by subclassing the error object and making all properties not enumerable
-    private static patchError() {
-        var orig = global.Error;
-
-        // New error handler
-        function AppInsightsAsyncCorrelatedErrorWrapper() {
-            if (!(this instanceof AppInsightsAsyncCorrelatedErrorWrapper)) {
-                return AppInsightsAsyncCorrelatedErrorWrapper.apply(Object.create(AppInsightsAsyncCorrelatedErrorWrapper.prototype), arguments);
-            }
-
-            // Is this object set to rewrite the stack?
-            // If so, we should turn off some Zone stuff that is prone to break
-            var stackRewrite = (<any>orig).stackRewrite;
-            if ((<any>orig).prepareStackTrace) {
-                (<any>orig).stackRewrite= false;
-                var stackTrace = (<any>orig).prepareStackTrace;
-                (<any>orig).prepareStackTrace = (e: any, s: any[]) => {
-                    // Remove some AI and Zone methods from the stack trace
-                    // Otherwise we leave side-effects
-
-                    // Algorithm is to find the first frame on the stack after the first instance(s)
-                    // of AutoCollection/CorrelationContextManager
-                    // Eg. this should return the User frame on an array like below:
-                    //  Zone | Zone | CorrelationContextManager | CorrelationContextManager | User
-                    var foundOne = false;
-                    for (var i=0; i<s.length; i++) {
-                        let fileName = s[i].getFileName();
-                        if (fileName) {
-                            if (fileName.indexOf("AutoCollection/CorrelationContextManager") === -1 &&
-                                fileName.indexOf("AutoCollection\\CorrelationContextManager") === -1) {
-
-                                if (foundOne) {
-                                    break;
-                                }
-                            } else {
-                                foundOne = true;
-                            }
-                        }
-                    }
-                    // Loop above goes one extra step
-                    i = Math.max(0, i - 1);
-
-                    if (foundOne) {
-                        s.splice(0, i);
-                    }
-                    return stackTrace(e, s);
-                }
-            }
-
-            // Apply the error constructor
-            orig.apply(this, arguments);
-
-            // Restore Zone stack rewriting settings
-            (<any>orig).stackRewrite = stackRewrite;
-
-            // Remove unexpected bits from stack trace
-            if (this.stack && typeof this.stack === "string") {
-                var stackFrames: string[] = (<string>this.stack).split("\n");
-                // Remove this class
-                if (stackFrames.length > 3) {
-                    if (stackFrames[2].trim().indexOf("at Error.AppInsightsAsyncCorrelatedErrorWrapper") === 0) {
-                        stackFrames.splice(2, 1);
-                    } else if (stackFrames[1].trim().indexOf("at AppInsightsAsyncCorrelatedErrorWrapper.ZoneAwareError") === 0
-                        && stackFrames[2].trim().indexOf("at new AppInsightsAsyncCorrelatedErrorWrapper") === 0) {
-                        stackFrames.splice(1, 2);
-                    }
-                }
-                // Remove AI correlation ids
-                this.stack = stackFrames.map((v) => {
-                    let startIndex = v.indexOf(") [");
-                    if (startIndex > -1) {
-                        v = v.substr(0, startIndex + 1);
-                    }
-                    return v;
-                }).join("\n");
-            }
-
-
-            // getOwnPropertyNames should be a superset of Object.keys...
-            // This appears to not always be the case
-            var props = Object.getOwnPropertyNames(this).concat(Object.keys(this));
-
-            // Zone.js will automatically create some hidden properties at read time.
-            // We need to proactively make those not enumerable as well as the currently visible properties
-            for(var i=0; i < props.length; i++) {
-                var propertyName = props[i];
-                var hiddenPropertyName = (<any>Zone)['__symbol__'](propertyName);
-                Object.defineProperty(this, propertyName, { enumerable: false });
-                Object.defineProperty(this, hiddenPropertyName, { enumerable: false, writable: true });
-            }
-
-            return this;
-        }
-
-        // Inherit from the Zone.js error handler
-        AppInsightsAsyncCorrelatedErrorWrapper.prototype = orig.prototype;
-
-        // We need this loop to copy outer methods like Error.captureStackTrace
-        var props = Object.getOwnPropertyNames(orig);
-        for(var i=0; i < props.length; i++) {
-            var propertyName = props[i];
-            if (!(<any>AppInsightsAsyncCorrelatedErrorWrapper)[propertyName]) {
-                Object.defineProperty(AppInsightsAsyncCorrelatedErrorWrapper, propertyName, Object.getOwnPropertyDescriptor(orig, propertyName));
-            }
-        }
-
-        // explicit cast to <any> required to avoid type error for captureStackTrace
-        // with latest node.d.ts (despite workaround above)
-        global.Error = <any>AppInsightsAsyncCorrelatedErrorWrapper;
     }
 }
 
