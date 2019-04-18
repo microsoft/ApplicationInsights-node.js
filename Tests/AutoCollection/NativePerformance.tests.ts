81f4a9816ba89f3f1ac6918694c4e8d729df4cc1
Mark Wolff <marwolff@microsoft.com>
2019-04-17 18:11:16 -0700

    native: add throw tests

--- Tests/AutoCollection/NativePerformance.tests.ts
+++ Tests/AutoCollection/NativePerformance.tests.ts
@@ -2,6 +2,8 @@ import assert = require("assert");
 import sinon = require("sinon");
 
 import AppInsights = require("../../applicationinsights");
+import TelemetryClient = require("../../Library/TelemetryClient");
+import AutoCollectNativePerformance = require("../../AutoCollection/NativePerformance");
 
 describe("AutoCollection/NativePerformance", () => {
     afterEach(() => {
@@ -21,6 +23,29 @@ describe("AutoCollection/NativePerformance", () => {
 
             setIntervalSpy.restore();
             clearIntervalSpy.restore();
+        });
+
+        it("constructor should be safe to call multiple times", () => {
+            var client = new TelemetryClient("key");
+            var native = new AutoCollectNativePerformance(client);
+            var disposeSpy = sinon.spy(AutoCollectNativePerformance.INSTANCE, "dispose");
+
+            assert.ok(native);
+            assert.ok(disposeSpy.notCalled);
+
+            assert.doesNotThrow(() => {native = new AutoCollectNativePerformance(client)}, "NativePerformance can be constructed more than once");
+            assert.ok(disposeSpy.calledOnce, "dispose is called when second instance is constructed");
+        });
+
+        it("Calling enable when metrics are not available should fail gracefully", () => {
+            var client = new TelemetryClient("key");
+            var native = new AutoCollectNativePerformance(client);
+
+            AutoCollectNativePerformance["_metricsAvailable"] = false;
+            assert.ok(!(<any>native)["_emitter"]);
+
+            assert.doesNotThrow(() => native.enable(true), "Does not throw when native metrics are not available and trying to enable");
+            assert.doesNotThrow(() => native.enable(false), "Does not throw when native metrics are not available and trying to disable");
         });
     });
 });
