688644c3bb1dc63555abb5c9d83d8dd845499965
Mark Wolff <marwolff@microsoft.com>
2019-08-30 11:11:51 -0700

    refactor: use telemetry type consts

--- TelemetryProcessors/PerformanceMetricsTelemetryProcessor.ts
+++ TelemetryProcessors/PerformanceMetricsTelemetryProcessor.ts
@@ -2,6 +2,7 @@ import Contracts = require("../Declarations/Contracts");
 import Logging = require("../Library/Logging");
 import QuickPulseStateManager = require("../Library/QuickPulseStateManager")
 import AutoCollectPerformance = require("../AutoCollection/Performance");
+import * as TelemetryType from "../Declarations/Contracts";
 
 export function performanceMetricsTelemetryProcessor(envelope: Contracts.Envelope, client?: QuickPulseStateManager): boolean {
     // If live metrics is enabled, forward all telemetry there
@@ -11,14 +12,14 @@ export function performanceMetricsTelemetryProcessor(envelope: Contracts.Envelop
 
     // Increment rate counters (for standard metrics and live metrics)
     switch (envelope.data.baseType) {
-        case "ExceptionData":
+        case TelemetryType.TelemetryTypeString.Exception:
             AutoCollectPerformance.countException();
             break;
-        case "RequestData":
+        case TelemetryType.TelemetryTypeString.Request:
             const requestData: Contracts.RequestData = (envelope.data as any).baseData;
             AutoCollectPerformance.countRequest(requestData.duration, requestData.success);
             break;
-        case "RemoteDependencyData":
+        case TelemetryType.TelemetryTypeString.Dependency:
             const remoteDependencyData: Contracts.RemoteDependencyData = (envelope.data as any).baseData;
             AutoCollectPerformance.countDependency(remoteDependencyData.duration, remoteDependencyData.success);
             break;
