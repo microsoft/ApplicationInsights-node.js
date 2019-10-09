b23211bd53547d5a4a5cc5e3298d1432baa2993c
Socolin <provost.bertrand@gmail.com>
2019-10-09 12:23:01 -0400

    feat: Allow to specify severityLevel when tracking Exception

--- Declarations/Contracts/TelemetryTypes/ExceptionTelemetry.ts
+++ Declarations/Contracts/TelemetryTypes/ExceptionTelemetry.ts
@@ -1,4 +1,5 @@
 import { Telemetry }  from "./Telemetry";
+import Contracts = require("../");
 
 /**
  * Telemetry about the exception thrown by the application
@@ -14,4 +15,8 @@ export interface ExceptionTelemetry extends Telemetry
      * Metrics associated with this exception, displayed in Metrics Explorer on the portal. Defaults to empty
      */
     measurements?: { [key: string]: number; };
+    /**
+     * Exception severity level
+     */
+    severity?: Contracts.SeverityLevel;
 }
