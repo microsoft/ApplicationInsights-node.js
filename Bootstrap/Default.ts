58f68b515acf0b8f934169e4c76b0f423d6846e0
Mark Wolff <marwolff@microsoft.com>
2019-12-11 15:04:29 -0800

    feat: implement sdkAlreadyExists

--- Bootstrap/Default.ts
+++ Bootstrap/Default.ts
@@ -14,6 +14,26 @@ export interface AgentLogger {
     error(message?: any, ...optional: any[]): void;
 }
 
+function sdkAlreadyExists(): boolean {
+    try {
+        // appInstance should either resolve to user SDK or crash. If it resolves to attach SDK, user probably modified their NODE_PATH
+        const appInstance = require.resolve("applicationinsights"); // assumes that the cwd is near user's package.json
+        const attachInstance = require.resolve("../applicationinsights");
+        if (appInstance !== attachInstance) {
+            _logger.log(
+                "applicationinsights module is already installed in this application; not re-attaching. Installed SDK location:",
+                appInstance
+            );
+            return true;
+        }
+        // User probably modified their NODE_PATH to resolve to this instance. Attach appinsights
+        return false;
+    } catch (e) {
+        // crashed while trying to resolve "applicationinsights", so SDK does not exist. Attach appinsights
+        return false;
+    }
+}
+
 /**
  * Sets the attach-time logger
  * @param logger logger which implements the `AgentLogger` interface
@@ -35,7 +55,7 @@ export function setUsagePrefix(prefix: string) {
  * @param setupString connection string or instrumentation key
  */
 export function setupAndStart(setupString = _setupString): typeof types | null {
-    if (!_extensionEnabled) {
+    if (!_extensionEnabled && sdkAlreadyExists() === false) {
         return null;
     }
     if (!setupString) {
