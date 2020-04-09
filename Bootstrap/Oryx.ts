192d4bd6412037429f3a97ae2b31334f6beb04a3
Mark Wolff <mark.wolff@microsoft.com>
2020-04-09 13:31:31 -0400

    bump 1.7.4

--- Bootstrap/Oryx.ts
+++ Bootstrap/Oryx.ts
@@ -1,5 +1,3 @@
-import * as types from "../applicationinsights";
-import { FileWriter } from "./FileWriter";
 import { StatusLogger } from "./StatusLogger";
 import { DiagnosticLogger } from "./DiagnosticLogger";
 import appInsightsLoader = require("./Default");
@@ -7,18 +5,10 @@ import appInsightsLoader = require("./Default");
 appInsightsLoader.setUsagePrefix("alr_"); // App Services Linux Attach
 
 // Set Status.json logger
-appInsightsLoader.setStatusLogger(new StatusLogger(new FileWriter(
-    StatusLogger.DEFAULT_FILE_PATH,
-    StatusLogger.DEFAULT_FILE_NAME,
-    { deleteOnExit: true, renamePolicy: "stop" }
-)));
+appInsightsLoader.setStatusLogger(new StatusLogger(console));
 
 // Set Attach Diagnostic Logger
-appInsightsLoader.setLogger(new DiagnosticLogger(new FileWriter(
-    DiagnosticLogger.DEFAULT_LOG_DIR,
-    DiagnosticLogger.DEFAULT_FILE_NAME,
-    { append: true, deleteOnExit: true, renamePolicy: "stop" }
-)));
+appInsightsLoader.setLogger(new DiagnosticLogger(console));
 
 // Start the SDK
 var appInsights = appInsightsLoader.setupAndStart();
