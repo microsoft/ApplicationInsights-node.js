3d3a998edd65d95c5d991d80ea8edff647ef5b31
Mark Wolff <marwolff@microsoft.com>
2020-07-30 17:21:42 -0400

    agent: use noop logger (#659)

--- Bootstrap/Oryx.ts
+++ Bootstrap/Oryx.ts
@@ -1,15 +1,16 @@
 import * as types from "../applicationinsights"; // needed but unused
 import { StatusLogger } from "./StatusLogger";
 import { DiagnosticLogger } from "./DiagnosticLogger";
+import { NoopLogger } from "./NoopLogger";
 import appInsightsLoader = require("./Default");
 
 appInsightsLoader.setUsagePrefix("alr_"); // App Services Linux Attach
 
 // Set Status.json logger
-appInsightsLoader.setStatusLogger(new StatusLogger(console));
+appInsightsLoader.setStatusLogger(new StatusLogger(new NoopLogger()));
 
 // Set Attach Diagnostic Logger
-appInsightsLoader.setLogger(new DiagnosticLogger(console));
+appInsightsLoader.setLogger(new DiagnosticLogger(new NoopLogger()));
 
 // Start the SDK
 var appInsights = appInsightsLoader.setupAndStart();
