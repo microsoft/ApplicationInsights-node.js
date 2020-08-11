b53c42567a04b51c7551f7d42458af5b5876e661
kryalama <kryalama@microsoft.com>
2020-08-11 09:53:28 -0700

    updating warn message to pass ikey

--- Library/Config.ts
+++ Library/Config.ts
@@ -74,7 +74,7 @@ class Config {
         this.instrumentationKey = csCode.instrumentationkey || iKeyCode /* === instrumentationKey */ || csEnv.instrumentationkey || Config._getInstrumentationKey();
         // validate ikey. If fails throw a warning
         if(!Config._validateInstrumentationKey(this.instrumentationKey)) {
-            Logging.warn("invalid instrumentation key - "+this.instrumentationKey);
+            Logging.warn("An invalid instrumentation key was provided. There may be resulting telemetry loss", this.instrumentationKey);
         }
 
         this.endpointUrl = `${csCode.ingestionendpoint || csEnv.ingestionendpoint || this.endpointBase}/v2/track`;
@@ -141,7 +141,17 @@ class Config {
 
     /**
     * Validate UUID Format
-    * Specs taken from https://tools.ietf.org/html/rfc4122
+    * Specs taken from breeze repo(https://dev.azure.com/msazure/One/_git/Mgmt-AppInsights-Breeze)
+    * The definition of a VALID instrumentation key is as follows:
+    * Not none
+    * Not empty
+    * Every character is a hex character [0-9a-f]
+    * 32 characters are separated into 5 sections via 4 dashes
+    * First section has 8 characters
+    * Second section has 4 characters
+    * Third section has 4 characters
+    * Fourth section has 4 characters
+    * Fifth section has 12 characters                  
     */    
     private static _validateInstrumentationKey(iKey:string): boolean {
         const UUID_Regex = '^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$';
