34d00486429b788b41a451afecd171fdaaeb1442
Mark Wolff <marwolff@microsoft.com>
2020-09-08 16:09:51 -0700

    [Codeless] change format of diag logger output (#678)
    * change format of diag logger output
    
    * add rest of property tests

---
+++ Tests/Bootstrap/DiagnosticLogger.spec.ts
@@ -0,0 +1,55 @@
+import assert = require("assert");
+import sinon = require("sinon");
+import { AgentLogger } from "../../Bootstrap/DataModel";
+import { DiagnosticLogger } from "../../Bootstrap/DiagnosticLogger";
+import { NoopLogger } from "../../Bootstrap/NoopLogger";
+import * as DataModel from "../../Bootstrap/DataModel";
+
+class TestWriter implements AgentLogger {
+    prev: any;
+
+    log(message?: any, ...optional: any[]): void {
+        this.prev = message;
+    }
+
+    error(message?: any, ...optional: any[]): void {
+        this.log(message, ...optional);
+    }
+}
+
+describe("DiagnosticLogger", () => {
+    const logger = new DiagnosticLogger(new NoopLogger());
+    const stub = sinon.stub(logger["_writer"], "log");
+    const version = require("../../../package.json").version;
+
+    afterEach(() => {
+        stub.reset();
+    })
+
+    describe("#DiagnosticLogger.DefaultEnvelope", () => {
+        it("should have the correct version string", () => {
+            assert.equal(DiagnosticLogger.DefaultEnvelope.properties.sdkVersion, version);
+        });
+    });
+
+    describe("#DiagnosticLogger.logMessage", () => {
+        it("should log all required fields", () => {
+            logger.logMessage("Some message");
+            assert.deepEqual(stub.args[0][0], {
+                level: DataModel.SeverityLevel.INFO,
+                message: "Some message",
+                logger: "applicationinsights.extension.diagnostics",
+                time: new Date().toISOString(),
+                properties: {
+                    language: "nodejs",
+                    operation: "Startup",
+                    siteName: undefined,
+                    ikey: undefined,
+                    extensionVersion: undefined,
+                    sdkVersion: version,
+                    subscriptionId: null,
+                }
+            } as DataModel.DiagnosticLog)
+        })
+    });
+});
