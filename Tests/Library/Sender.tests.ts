c13a78d900be17f000992c2a2fbfb162062c5271
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2021-04-02 14:38:24 -0700

    Adding Breeze endpoint redirect support (#747)
    * Adding Breeze endpoint redirect support
    
    * Typo
    
    * Back compatibility
    
    * Use http interface
    
    * Node.js 8 latest is supported
    
    * Update nock version for latest supporting node.js 8

--- Tests/Library/Sender.tests.ts
+++ Tests/Library/Sender.tests.ts
@@ -1,7 +1,10 @@
 import assert = require("assert");
+import sinon = require("sinon");
+import nock = require("nock");
 
 import Sender = require("../../Library/Sender");
 import Config = require("../../Library/Config");
+import Constants = require("../../Declarations/Constants");
 
 class SenderMock extends Sender {
     public getResendInterval() {
@@ -10,7 +13,7 @@ class SenderMock extends Sender {
 }
 
 describe("Library/Sender", () => {
-    var sender:SenderMock;
+    var sender: SenderMock;
 
     beforeEach(() => {
         sender = new SenderMock(new Config("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333"));
@@ -37,5 +40,76 @@ describe("Library/Sender", () => {
             sender.setDiskRetryMode(true, -1234);
             assert.equal(Sender.WAIT_BETWEEN_RESEND, sender.getResendInterval());
         });
+    });
+
+    describe("#endpoint redirect", () => {
+        var sandbox: sinon.SinonSandbox;
+        let interceptor: nock.Interceptor;
+
+        before(() => {
+            interceptor = nock(Constants.DEFAULT_BREEZE_ENDPOINT)
+                .post("/v2/track", (body: string) => {
+                    return true;
+                });
+        });
+
+        beforeEach(() => {
+            sandbox = sinon.sandbox.create();
+        });
+
+        afterEach(() => {
+            sandbox.restore();
+        });
+
+        after(() => {
+            nock.cleanAll();
+        });
+
+        it("should change ingestion endpoint when redirect response code is returned (301)", (done) => {
+            interceptor.reply(301, {}, { "Location": "testLocation" });
+            var testSender = new Sender(new Config("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333"));
+            testSender.send(new Buffer("test"), (responseText) => {
+                assert.equal(testSender["_redirectedHost"], "testLocation");
+                done();
+            });
+        });
+
+        it("should change ingestion endpoint when redirect response code is returned (308)", (done) => {
+            interceptor.reply(308, {}, { "Location": "testLocation" });
+            var testSender = new Sender(new Config("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333"));
+            testSender.send(new Buffer("test"), (responseText) => {
+                assert.equal(testSender["_redirectedHost"], "testLocation");
+                done();
+            });
+        });
+
+        it("should not change ingestion endpoint if redirect is not triggered", (done) => {
+            interceptor.reply(200, {}, { "Location": "testLocation" });
+            var testSender = new Sender(new Config("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333"));
+            testSender.send(new Buffer("test"), (responseText) => {
+                assert.equal(testSender["_redirectedHost"], null);
+                done();
+            });
+        });
+
+        it("should use redirect URL for following requests", (done) => {
+            let redirectHost = "https://testLocation";
+            let redirectLocation = redirectHost + "/v2/track";
+            // Fake redirect endpoint
+            nock(redirectHost)
+                .post("/v2/track", (body: string) => {
+                    return true;
+                }).reply(200, { "redirectProperty": true });
+            interceptor.reply(308, {}, { "Location": redirectLocation });
+            var testSender = new Sender(new Config("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333"));
+            testSender.send(new Buffer("test"), () => {
+                assert.equal(testSender["_redirectedHost"], redirectLocation);
+                testSender.send(new Buffer("test"), (responseText) => {
+                    assert.equal(responseText, '{"redirectProperty":true}');
+                    done();
+                });
+            });
+        });
+
     });
 });
