9e47d47074d3f33e2a0c9c34de1bec9f1d26f433
Hector Hernandez Guzman <hectorh@microsoft.com>
2021-05-18 16:26:55 -0700

    Fix js test

--- Tests/js/endToEnd.js
+++ Tests/js/endToEnd.js
@@ -1,15 +1,20 @@
 const assert = require('assert');
+const https = require('https');
 const sinon = require('sinon');
 
 // Special embedded test cases for testing if app can close
 if (process.argv.indexOf('embeddedTestCase-AppTerminates1') > -1) {
     var appInsights = require('../..');
-    appInsights.setup("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333").start();
+    appInsights.setup("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333");
+    appInsights.defaultClient.config.httpsAgent = new https.Agent({ keepAlive: false });
+    appInsights.start();
     return;
 } else if (process.argv.indexOf('embeddedTestCase-AppTerminates2') > -1) {
     var appInsights = require('../..');
-    appInsights.setup("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333").start();
-    appInsights.defaultClient.trackEvent({name: 'testEvent'});
+    appInsights.setup("1aa11111-bbbb-1ccc-8ddd-eeeeffff3333");
+    appInsights.defaultClient.config.httpsAgent = new https.Agent({ keepAlive: false });
+    appInsights.start();
+    appInsights.defaultClient.trackEvent({ name: 'testEvent' });
     appInsights.defaultClient.flush();
     return;
 }
@@ -17,24 +22,24 @@ if (process.argv.indexOf('embeddedTestCase-AppTerminates1') > -1) {
 describe('module', function () {
     describe('#require', function () {
         it('loads the applicationinsights module', function (done) {
-            assert.doesNotThrow(function() { return require('../..') });
+            assert.doesNotThrow(function () { return require('../..') });
             done();
         });
     });
-    describe('applicationinsights', function() {
+    describe('applicationinsights', function () {
         if (/^0\.(([0-9]\.)|(10\.))/.test(process.versions.node)) {
             // These tests aren't valid in Node 0.10
             return;
         }
         it('does not prevent the app from terminating if started', function (done) {
-            this.timeout(10000);
+            this.timeout(5000);
             var testCase = require('child_process').fork(__filename, ['embeddedTestCase-AppTerminates1']);
-            var timer = setTimeout(function(){
+            var timer = setTimeout(function () {
                 assert(false, "App failed to terminate!");
                 testCase.kill();
                 done();
-            }, 10000);
-            testCase.on("close", function() {
+            }, 5000);
+            testCase.on("close", function () {
                 clearTimeout(timer);
                 done();
             });
@@ -43,12 +48,12 @@ describe('module', function () {
         it('does not prevent the app from terminating if started and called track and flush', function (done) {
             this.timeout(10000);
             var testCase = require('child_process').fork(__filename, ['embeddedTestCase-AppTerminates2']);
-            var timer = setTimeout(function(){
+            var timer = setTimeout(function () {
                 assert(false, "App failed to terminate!");
                 testCase.kill();
                 done();
             }, 10000);
-            testCase.on("close", function() {
+            testCase.on("close", function () {
                 clearTimeout(timer);
                 done();
             });
@@ -58,7 +63,9 @@ describe('module', function () {
     describe('rejected promises', function () {
         it('should not crash on rejected promises containing no callstack', function () {
             var appInsights = require('../../');
-            appInsights.setup('1aa11111-bbbb-1ccc-8ddd-eeeeffff3333').start();
+            appInsights.setup('1aa11111-bbbb-1ccc-8ddd-eeeeffff3333');
+            appInsights.defaultClient.config.httpsAgent = new https.Agent({ keepAlive: false });
+            appInsights.start();
             assert.ok(appInsights.defaultClient);
             assert.doesNotThrow(function () {
                 if (typeof Promise !== 'undefined') {
@@ -70,38 +77,40 @@ describe('module', function () {
         });
     });
 
-    describe('uncaught exceptions', function() {
+    describe('uncaught exceptions', function () {
         var UNCAUGHT_EXCEPTION = 'uncaughtException';
         var UNCAUGHT_EXCEPTION_MONITOR = 'uncaughtExceptionMonitor';
         var exitStub;
         var mochaListener;
 
-        var getLegacyHandler = function() {
+        var getLegacyHandler = function () {
             return process.listeners(UNCAUGHT_EXCEPTION)[0];
         }
 
-        var getMonitorHandler = function() {
+        var getMonitorHandler = function () {
             return process.listeners(UNCAUGHT_EXCEPTION_MONITOR)[0];
         }
 
-        before(function() {
+        before(function () {
             mochaListener = process.listeners(UNCAUGHT_EXCEPTION).pop();
             process.removeListener(UNCAUGHT_EXCEPTION, mochaListener);
             exitStub = sinon.stub(process, 'exit');
         });
 
-        beforeEach(function() {
+        beforeEach(function () {
             exitStub.reset();
         });
 
-        after(function() {
+        after(function () {
             process.addListener(UNCAUGHT_EXCEPTION, mochaListener);
             exitStub.restore();
         });
 
         it('should crash on uncaught exceptions', function () {
             var appInsights = require('../../');
-            appInsights.setup('1aa11111-bbbb-1ccc-8ddd-eeeeffff3333').setAutoCollectExceptions(true).start();
+            appInsights.setup('1aa11111-bbbb-1ccc-8ddd-eeeeffff3333').setAutoCollectExceptions(true);
+            appInsights.defaultClient.config.httpsAgent = new https.Agent({ keepAlive: false });
+            appInsights.start();
             assert.ok(appInsights.defaultClient);
             var handler;
             if (handler = getLegacyHandler()) {
@@ -119,11 +128,13 @@ describe('module', function () {
 
         it('should not crash on uncaught exceptions if multiple handlers exist', function () {
             var appInsights = require('../../');
-            appInsights.setup('1aa11111-bbbb-1ccc-8ddd-eeeeffff3333').setAutoCollectExceptions(true).start();
-            process.addListener(UNCAUGHT_EXCEPTION, function() {});
+            appInsights.setup('1aa11111-bbbb-1ccc-8ddd-eeeeffff3333').setAutoCollectExceptions(true);
+            appInsights.defaultClient.config.httpsAgent = new https.Agent({ keepAlive: false });
+            appInsights.start();
+            process.addListener(UNCAUGHT_EXCEPTION, function () { });
             assert.ok(appInsights.defaultClient);
 
-            assert.doesNotThrow(function() {
+            assert.doesNotThrow(function () {
                 var handler = getMonitorHandler() || getLegacyHandler();
                 handler(new Error('existing handler error'));
                 assert.equal(exitStub.callCount, 0, 'Handler does not rethrow when other handles exist');
