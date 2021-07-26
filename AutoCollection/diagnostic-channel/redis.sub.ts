3b0fba9265284d5902f5794c23f159518e215309
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2021-07-26 13:00:53 -0700

    Fixing accidental reverts (#798)
    * Adding StatsBeat implementation
    
    * Update
    
    * Addressing some comments
    
    * WIP
    
    * WIP
    
    * Adding tests
    
    * Update
    
    * Addressing comments
    
    * Fix test

--- AutoCollection/diagnostic-channel/redis.sub.ts
+++ AutoCollection/diagnostic-channel/redis.sub.ts
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for details.
 import TelemetryClient = require("../../Library/TelemetryClient");
+import { StatsbeatInstrumentation } from "../../Declarations/Constants";
 import { channel, IStandardEvent } from "diagnostic-channel";
 
 import { redis } from "diagnostic-channel-publishers";
@@ -30,15 +31,22 @@ export const subscriber = (event: IStandardEvent<redis.IRedisData>) => {
 };
 
 export function enable(enabled: boolean, client: TelemetryClient) {
+    let statsbeat = client.getStatsbeat();
     if (enabled) {
         if (clients.length === 0) {
             channel.subscribe<redis.IRedisData>("redis", subscriber);
+            if (statsbeat) {
+                statsbeat.addInstrumentation(StatsbeatInstrumentation.REDIS);
+            }
         };
         clients.push(client);
     } else {
         clients = clients.filter((c) => c != client);
         if (clients.length === 0) {
             channel.unsubscribe("redis", subscriber);
+            if (statsbeat) {
+                statsbeat.removeInstrumentation(StatsbeatInstrumentation.REDIS);
+            }
         }
     }
 }
