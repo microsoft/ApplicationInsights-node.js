a4a994f6a592f38e63df836b09fe77ea9790e1cb
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2021-10-25 13:30:27 -0700

    Check for clients in auto instrumentation subs (#858)

--- AutoCollection/diagnostic-channel/postgres.sub.ts
+++ AutoCollection/diagnostic-channel/postgres.sub.ts
@@ -30,6 +30,10 @@ export const subscriber = (event: IStandardEvent<pg.IPostgresData>) => {
 export function enable(enabled: boolean, client: TelemetryClient) {
     let statsbeat = client.getStatsbeat();
     if (enabled) {
+        let clientFound = clients.find(c => c == client);
+        if (clientFound) {
+            return;
+        }
         if (clients.length === 0) {
             channel.subscribe<pg.IPostgresData>("postgres", subscriber);
             if (statsbeat) {
