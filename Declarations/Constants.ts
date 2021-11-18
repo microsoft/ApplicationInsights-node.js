9ecabf08b7f4ad6a6bf48340bd9ca1fcd7d12fec
Hector Hernandez Guzman <hectorh@microsoft.com>
2021-11-18 15:42:10 -0800

    Fix name in statsbeat metrics

--- Declarations/Constants.ts
+++ Declarations/Constants.ts
@@ -147,7 +147,7 @@ export const StatsbeatAttach = {
 
 export const StatsbeatCounter = {
     REQUEST_SUCCESS: "Request Success Count",
-    REQUEST_FAILURE: "Requests Failure Count",
+    REQUEST_FAILURE: "Request Failure Count",
     REQUEST_DURATION: "Request Duration",
     RETRY_COUNT: "Retry Count",
     THROTTLE_COUNT: "Throttle Count",
