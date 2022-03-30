798f40371e1f272822a88d3fbafb9eb4713e30a4
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2022-03-30 13:21:51 -0700

    WIP

--- Declarations/Contracts/TelemetryTypes/index.ts
+++ Declarations/Contracts/TelemetryTypes/index.ts
@@ -5,11 +5,9 @@ export * from "./MetricTelemetry";
 export * from "./RequestTelemetry";
 export * from "./TraceTelemetry";
 export * from "./Telemetry";
-
 export * from "./NodeHttpDependencyTelemetry";
 export * from "./NodeHttpRequestTelemetry";
 export * from "./AvailabilityTelemetry";
 export * from "./PageViewTelemetry";
-
-export * from "./EnvelopeTelemetry";
 export * from "./TelemetryType";
+
