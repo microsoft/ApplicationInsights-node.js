02ecd41f3dcae99bc7b9ab4a572084a632efd867
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2022-09-09 14:55:53 -0700

    Update

--- Bootstrap/Default.ts
+++ Bootstrap/Default.ts
@@ -40,6 +40,38 @@ export function setStatusLogger(statusLogger: StatusLogger) {
     _statusLogger = statusLogger;
 }
 
+export function sdkAlreadyExists(): boolean {
+    try {
+        // appInstance should either resolve to user SDK or crash. If it resolves to attach SDK, user probably modified their NODE_PATH
+        let appInstance: string;
+        try {
+            // Node 8.9+
+            appInstance = (require.resolve as any)("applicationinsights", { paths: [process.cwd()] });
+        } catch (e) {
+            // Node <8.9
+            appInstance = require.resolve(process.cwd() + "/node_modules/applicationinsights");
+        }
+        // If loaded instance is in Azure machine home path do not attach the SDK, this means customer already instrumented their app
+        if (appInstance.indexOf("home") > -1) {
+            const diagnosticLog: DiagnosticLog = {
+                message: "Application Insights SDK already exists. Module is already installed in this application; not re-attaching. Installed SDK location: " + appInstance,
+                properties: {
+                    "msgId": DiagnosticMessageId.sdkExists
+                }
+            };
+            _logger.logError(diagnosticLog);
+            return true;
+        }
+        else {
+            // ApplicationInsights could be loaded outside of customer application, attach in this case
+            return false;
+        }
+    } catch (e) {
+        // crashed while trying to resolve "applicationinsights", so SDK does not exist. Attach appinsights
+        return false;
+    }
+}
+
 /**
  * Try to setup and start this app insights instance if attach is enabled.
  * @param aadTokenCredential Optional AAD credential
@@ -164,37 +196,5 @@ export function setupAndStart(aadTokenCredential?: azureCore.TokenCredential): t
         })
     }
     return _appInsights;
-}
-
-export function sdkAlreadyExists(): boolean {
-    try {
-        // appInstance should either resolve to user SDK or crash. If it resolves to attach SDK, user probably modified their NODE_PATH
-        let appInstance: string;
-        try {
-            // Node 8.9+
-            appInstance = (require.resolve as any)("applicationinsights", { paths: [process.cwd()] });
-        } catch (e) {
-            // Node <8.9
-            appInstance = require.resolve(process.cwd() + "/node_modules/applicationinsights");
-        }
-        // If loaded instance is in Azure machine home path do not attach the SDK, this means customer already instrumented their app
-        if (appInstance.indexOf("home") > -1) {
-            const diagnosticLog: DiagnosticLog = {
-                message: "Application Insights SDK already exists. Module is already installed in this application; not re-attaching. Installed SDK location: " + appInstance,
-                properties: {
-                    "msgId": DiagnosticMessageId.sdkExists
-                }
-            };
-            _logger.logError(diagnosticLog);
-            return true;
-        }
-        else {
-            // ApplicationInsights could be loaded outside of customer application, attach in this case
-            return false;
-        }
-    } catch (e) {
-        // crashed while trying to resolve "applicationinsights", so SDK does not exist. Attach appinsights
-        return false;
-    }
 }
 
