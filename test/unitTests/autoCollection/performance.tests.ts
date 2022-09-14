8e7d1b67a3f946d239aef28847846df8782298aa
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2022-09-14 14:02:22 -0700

    Updating test

--- test/unitTests/autoCollection/performance.tests.ts
+++ test/unitTests/autoCollection/performance.tests.ts
@@ -1,10 +1,9 @@
-import { MetricData } from "@opentelemetry/sdk-metrics";
+import { SpanKind } from "@opentelemetry/api";
 import * as assert from "assert";
 import * as sinon from "sinon";
 import { PerformanceCounterMetricsHandler } from "../../../src/autoCollection";
-import { NativeMetricsCounter, PerformanceCounter, QuickPulseCounter } from "../../../src/autoCollection/metrics/types";
+import { NativeMetricsCounter, PerformanceCounter } from "../../../src/autoCollection/metrics/types";
 import { Config } from "../../../src/library/configuration";
-import { MetricHandler } from "../../../src/library/handlers";
 
 
 describe("PerformanceCounterMetricsHandler", () => {
@@ -43,27 +42,36 @@ describe("PerformanceCounterMetricsHandler", () => {
         it("should observe instruments during collection", async () => {
             let mockExport = sandbox.stub(autoCollect["_azureExporter"], "export");
             autoCollect.start();
+            autoCollect.getHttpMetricsInstrumentation().setMeterProvider(autoCollect["_meterProvider"]);
+            autoCollect.getHttpMetricsInstrumentation()["_httpRequestDone"]({
+                startTime: Date.now() - 100,
+                isProcessed: false,
+                spanKind: SpanKind.SERVER,
+                attributes: { "HTTP_STATUS_CODE": "200" }
+            });
+
             await new Promise(resolve => setTimeout(resolve, 120));
             assert.ok(mockExport.called);
             let resourceMetrics = mockExport.args[0][0];
             const scopeMetrics = resourceMetrics.scopeMetrics;
-            assert.strictEqual(scopeMetrics.length, 1, 'scopeMetrics count');
-            const metrics = scopeMetrics[0].metrics;
-            assert.strictEqual(metrics.length, 13, 'metrics count');
+            assert.strictEqual(scopeMetrics.length, 2, 'scopeMetrics count');
+            let metrics = scopeMetrics[0].metrics;
+            assert.strictEqual(metrics.length, 12, 'metrics count');
             assert.equal(metrics[0].descriptor.name, PerformanceCounter.PRIVATE_BYTES);
             assert.equal(metrics[1].descriptor.name, PerformanceCounter.AVAILABLE_BYTES);
             assert.equal(metrics[2].descriptor.name, PerformanceCounter.PROCESSOR_TIME);
             assert.equal(metrics[3].descriptor.name, PerformanceCounter.PROCESS_TIME);
             assert.equal(metrics[4].descriptor.name, PerformanceCounter.REQUEST_RATE);
-            assert.equal(metrics[5].descriptor.name, PerformanceCounter.REQUEST_DURATION);
-
-            assert.equal(metrics[6].descriptor.name, NativeMetricsCounter.EVENT_LOOP_CPU);
-            assert.equal(metrics[7].descriptor.name, NativeMetricsCounter.GARBAGE_COLLECTION_SCAVENGE);
-            assert.equal(metrics[8].descriptor.name, NativeMetricsCounter.GARBAGE_COLLECTION_SWEEP_COMPACT);
-            assert.equal(metrics[9].descriptor.name, NativeMetricsCounter.GARBAGE_COLLECTION_INCREMENTAL_MARKING);
-            assert.equal(metrics[10].descriptor.name, NativeMetricsCounter.HEAP_MEMORY_TOTAL);
-            assert.equal(metrics[11].descriptor.name, NativeMetricsCounter.HEAP_MEMORY_USAGE);
-            assert.equal(metrics[12].descriptor.name, NativeMetricsCounter.MEMORY_USAGE_NON_HEAP);
+            assert.equal(metrics[5].descriptor.name, NativeMetricsCounter.EVENT_LOOP_CPU);
+            assert.equal(metrics[6].descriptor.name, NativeMetricsCounter.GARBAGE_COLLECTION_SCAVENGE);
+            assert.equal(metrics[7].descriptor.name, NativeMetricsCounter.GARBAGE_COLLECTION_SWEEP_COMPACT);
+            assert.equal(metrics[8].descriptor.name, NativeMetricsCounter.GARBAGE_COLLECTION_INCREMENTAL_MARKING);
+            assert.equal(metrics[9].descriptor.name, NativeMetricsCounter.HEAP_MEMORY_TOTAL);
+            assert.equal(metrics[10].descriptor.name, NativeMetricsCounter.HEAP_MEMORY_USAGE);
+            assert.equal(metrics[11].descriptor.name, NativeMetricsCounter.MEMORY_USAGE_NON_HEAP);
+            metrics = scopeMetrics[1].metrics;
+            assert.strictEqual(metrics.length, 1, 'metrics count');
+            assert.equal(metrics[0].descriptor.name, PerformanceCounter.REQUEST_DURATION);
         });
 
 
