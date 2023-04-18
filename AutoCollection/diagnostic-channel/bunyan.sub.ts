97965cc3b72f6e809e9e0826ef291a99c67702bf
Kelly Hilliard <kelly.hilliard@corrohealth.com>
2023-04-18 09:26:01 -0500

    Avoid logging duplicate exceptions

--- AutoCollection/diagnostic-channel/bunyan.sub.ts
+++ AutoCollection/diagnostic-channel/bunyan.sub.ts
@@ -23,6 +23,29 @@ const bunyanToAILevelMap: { [key: number]: number } = {
 const subscriber = (event: IStandardEvent<bunyan.IBunyanData>) => {
     let message = event.data.result as string;
     clients.forEach((client) => {
+        // For now, we will simply log everything as a trace.
+        //   The older 1.8.x code would actually never log an
+        //   exception anyway since message was NEVER of type
+        //   Error(shown here):
+        //
+        // var subscriber = function (event) {
+        //     var message = event.data.result;
+        //     clients.forEach(function (client) {
+        //         var AIlevel = bunyanToAILevelMap[event.data.level];
+        //         if (message instanceof Error) {
+        //             client.trackException({ exception: (message) });
+        //         }
+        //         else {
+        //             client.trackTrace({ message: message, severity: AIlevel });
+        //         }
+        //     });
+        // };
+        
+        // The way this code is written effectively breaks the Bunyan interface.
+        //   For now, bypass writing any exceptions.
+        //   For example, additional error detail is lost if you supplied a custom message.
+        //   https://github.com/trentm/node-bunyan#log-method-api
+        /*
         try {
             // Try to parse message as Bunyan log is JSON
             let log: any = JSON.parse(message);
@@ -31,12 +54,13 @@ const subscriber = (event: IStandardEvent<bunyan.IBunyanData>) => {
                 bunyanError.name = log.err.name;
                 bunyanError.stack = log.err.stack;
                 client.trackException({ exception: bunyanError });
-                // return;  For now, let's also log a trace message to avoid losing other properties of 'message'
+                return;
             }
         }
         catch (err) {
             // Ignore error
         }
+        */
         const AIlevel = bunyanToAILevelMap[event.data.level];
         client.trackTrace({ message: message, severity: AIlevel });
     });
