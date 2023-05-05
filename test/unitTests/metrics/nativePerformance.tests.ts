6fbd06ac683559748f9ca0e98aa06cd26d25b511
Hector <hectorjavierhdz@live.com>
2023-05-05 16:45:28 -0700

    Removing Native Metrics functionality

--- test/unitTests/metrics/nativePerformance.tests.ts
+++ test/unitTests/metrics/nativePerformance.tests.ts
@@ -1,44 +1,44 @@
-import * as assert from "assert";
-import * as sinon from "sinon";
-import { Meter } from "@opentelemetry/api";
-import { MeterProvider } from "@opentelemetry/sdk-metrics";
-import { NativePerformanceMetrics } from "../../../src/metrics/collection/nativePerformanceMetrics";
+// import * as assert from "assert";
+// import * as sinon from "sinon";
+// import { Meter } from "@opentelemetry/api";
+// import { MeterProvider } from "@opentelemetry/sdk-metrics";
+// import { NativePerformanceMetrics } from "../../../src/metrics/collection/nativePerformanceMetrics";
 
-class TestEmitter {
-    enable() { }
-    disable() { }
-    getLoopData() { }
-    getGCData() { }
-}
+// class TestEmitter {
+//     enable() { }
+//     disable() { }
+//     getLoopData() { }
+//     getGCData() { }
+// }
 
-describe("AutoCollection/NativePerformance", () => {
-    let sandbox: sinon.SinonSandbox;
-    let testMeter: Meter;
+// describe("AutoCollection/NativePerformance", () => {
+//     let sandbox: sinon.SinonSandbox;
+//     let testMeter: Meter;
 
-    before(() => {
-        sandbox = sinon.createSandbox();
-        const testProvider = new MeterProvider();
-        testMeter = testProvider.getMeter("test");
-    });
+//     before(() => {
+//         sandbox = sinon.createSandbox();
+//         const testProvider = new MeterProvider();
+//         testMeter = testProvider.getMeter("test");
+//     });
 
-    afterEach(() => {
-        sandbox.restore();
-    });
+//     afterEach(() => {
+//         sandbox.restore();
+//     });
 
-    describe("#Metrics", () => {
-        it("init should auto collection interval if native metrics packages is installed", () => {
-            const nativePerformance = new NativePerformanceMetrics(testMeter);
-            nativePerformance["_emitter"] = new TestEmitter();
-            assert.ok(!nativePerformance["_handle"]); // Package is not installed in test execution, TODO: Add test where this is available
-        });
+//     describe("#Metrics", () => {
+//         it("init should auto collection interval if native metrics packages is installed", () => {
+//             const nativePerformance = new NativePerformanceMetrics(testMeter);
+//             nativePerformance["_emitter"] = new TestEmitter();
+//             assert.ok(!nativePerformance["_handle"]); // Package is not installed in test execution, TODO: Add test where this is available
+//         });
 
-        it("Calling enable when metrics are not available should fail gracefully", () => {
-            let nativePerformance = null;
+//         it("Calling enable when metrics are not available should fail gracefully", () => {
+//             let nativePerformance = null;
 
-            assert.doesNotThrow(
-                () => nativePerformance = new NativePerformanceMetrics(testMeter),
-                "Does not throw when native metrics are not available"
-            );
-        });
-    });
-});
+//             assert.doesNotThrow(
+//                 () => nativePerformance = new NativePerformanceMetrics(testMeter),
+//                 "Does not throw when native metrics are not available"
+//             );
+//         });
+//     });
+// });
