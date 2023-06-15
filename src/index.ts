b30f6fd4a5239f994aecdad72d92afad15049236
Hector <hectorjavierhdz@live.com>
2023-06-15 14:11:44 -0700

    Update dependencies

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,13 @@
 
 export { ApplicationInsightsClient } from "./applicationInsightsClient";
 export { ApplicationInsightsConfig } from "./shared";
+export { LogHandler } from "./logs/logHandler";
+export { KnownSeverityLevel } from "./declarations/generated";
+export {
+    AvailabilityTelemetry,
+    TraceTelemetry,
+    ExceptionTelemetry,
+    EventTelemetry,
+    PageViewTelemetry,
+    Telemetry,
+} from "./declarations/contracts";
