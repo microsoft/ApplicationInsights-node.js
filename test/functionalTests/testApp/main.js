d60648c55ad643c243199c5aa2680a52883698f8
Hector <hectorjavierhdz@live.com>
2023-07-06 17:48:56 -0700

    Update funcitonal test initialization

--- test/functionalTests/testApp/main.js
+++ test/functionalTests/testApp/main.js
@@ -2,83 +2,107 @@ var testconfig = require("./config");
 var appInsights = null;
 if (testconfig.AppInsightsEnabled) {
 
-    const { ApplicationInsightsClient, ApplicationInsightsConfig } = require("applicationinsights");
+    const { TelemetryClient } = require("applicationinsights");
 
-    let config = new ApplicationInsightsConfig();
-    config.connectionString = `InstrumentationKey=${testconfig.InstrumentationKey};IngestionEndpoint=${testconfig.EndpointBaseAddress}`;
-    config.samplingRatio = parseFloat(testconfig.SampleRate);
-    config.instrumentations["http"].enabled = true;
-    config.instrumentations["azureSdk"].enabled = true;
-    config.instrumentations["mongoDb"].enabled = true;
-    config.instrumentations["mySql"].enabled = true;
-    config.instrumentations["postgreSql"].enabled = true;
-    config.instrumentations["redis"].enabled = true;
-    config.instrumentations["redis4"].enabled = true;
-    config.logInstrumentations.console.enabled = true;
-    config.logInstrumentations.bunyan.enabled = true;
-    config.logInstrumentations.winston.enabled = true;
-    config.enableAutoCollectExceptions = true;
-
-    appInsights = new ApplicationInsightsClient(config);
-}
-
-var Tasks = require("./tasks");
-var port = parseInt(testconfig.ServerPort);
-var bodyParser = require('body-parser');
-var express = require("express");
-var app = express();
-app.use(bodyParser.json());
-
-app.get("/", (req, res) => {
-    res.send("OK");
-});
-
-/**
- * Receive route configuration object of the following form as POST body:
- * [
- * {path: "/dependencyTest", steps:["HttpGet", "Timeout", "MongoInsert"]},
- * ...
- * ]
- *
- * This input will create routes on this server that perform those tasks.
- * The available tasks are defined in /Tasks/index.js
- */
-app.post("/_configure", (req, res) => {
-    var stepConfig = req.body;
-
-    var runTasks = (tasks, cb) => {
-        if (!tasks || tasks.length == 0) {
-            cb();
-            return;
+    let options = {
+        connectionString: `InstrumentationKey=${testconfig.InstrumentationKey};IngestionEndpoint=${testconfig.EndpointBaseAddress}`,
+        samplingRatio: parseFloat(testconfig.SampleRate),
+        instrumentationOptions: {
+            azureSdk: {
+                enabled: true
+            },
+            http: {
+                enabled: true
+            },
+            mongoDb: {
+                enabled: true
+            },
+            mySql: {
+                enabled: true
+            },
+            postgreSql: {
+                enabled: true
+            },
+            redis: {
+                enabled: true
+            },
+            redis4: {
+                enabled: true
+            }
+        },
+        logInstrumentations: {
+            console: {
+                enabled: true,
+            },
+            bunyan: {
+                enabled: true,
+            },
+            winston: {
+                enabled: true,
+            },
         }
-        tasks = tasks.slice(0);
-        var task = tasks.shift();
-        Tasks[task](() => runTasks(tasks, cb));
+    };
+
+    appInsights = new TelemetryClient(options);
     }
 
-    var generateStepRoute = (route) => {
-        app.get(route.path, (rq, rs) => {
-            runTasks(route.steps, () => rs.send("OK"));
-        });
-    }
+    var Tasks = require("./tasks");
+    var port = parseInt(testconfig.ServerPort);
+    var bodyParser = require('body-parser');
+    var express = require("express");
+    var app = express();
+    app.use(bodyParser.json());
 
-    for (var i = 0; i < stepConfig.length; i++) {
-        generateStepRoute(stepConfig[i]);
-    }
+    app.get("/", (req, res) => {
+        res.send("OK");
+    });
 
-    res.send("OK");
-});
+    /**
+     * Receive route configuration object of the following form as POST body:
+     * [
+     * {path: "/dependencyTest", steps:["HttpGet", "Timeout", "MongoInsert"]},
+     * ...
+     * ]
+     *
+     * This input will create routes on this server that perform those tasks.
+     * The available tasks are defined in /Tasks/index.js
+     */
+    app.post("/_configure", (req, res) => {
+        var stepConfig = req.body;
 
-app.get("/_close", (req, res) => {
-    res.end("OK");
-    server.close();
-    if (testconfig.AppInsightsEnabled) {
-        appInsights.flush();
-        appInsights.shutdown();
-        process.exit(0);
-    }
-});
+        var runTasks = (tasks, cb) => {
+            if (!tasks || tasks.length == 0) {
+                cb();
+                return;
+            }
+            tasks = tasks.slice(0);
+            var task = tasks.shift();
+            Tasks[task](() => runTasks(tasks, cb));
+        }
 
-var server = app.listen(port, () => {
-    console.log("TestApp Ready!");
-});
+        var generateStepRoute = (route) => {
+            app.get(route.path, (rq, rs) => {
+                runTasks(route.steps, () => rs.send("OK"));
+            });
+        }
+
+        for (var i = 0; i < stepConfig.length; i++) {
+            generateStepRoute(stepConfig[i]);
+        }
+
+        res.send("OK");
+    });
+
+    app.get("/_close", (req, res) => {
+        res.end("OK");
+        server.close();
+        if (testconfig.AppInsightsEnabled) {
+            appInsights.flush();
+            appInsights.shutdown();
+            process.exit(0);
+        }
+    });
+
+    var server = app.listen(port, () => {
+        console.log("TestApp Ready!");
+    });
