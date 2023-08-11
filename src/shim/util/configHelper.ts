18ad8ffb94815c8e68ddf4d00a3080c9d4924b6b
Jackson Weber <47067795+JacksonWeber@users.noreply.github.com>
2023-08-10 19:51:21 -0700

    Convert config helper to a collection of functions.

--- src/shim/util/configHelper.ts
+++ src/shim/util/configHelper.ts
@@ -2,80 +2,76 @@ import { HttpInstrumentationConfig } from "@opentelemetry/instrumentation-http";
 import { ApplicationInsightsOptions, ExtendedMetricType } from "../../types";
 import * as http from "http";
 
-class ConfigHelper {
-    static setAutoCollectPerformance(options: ApplicationInsightsOptions, value: boolean, collectExtendedMetrics?: any) {
-        if (options) {
-            options.enableAutoCollectPerformance = value;
-            if (typeof collectExtendedMetrics === "object") {
-                options.extendedMetrics = { ...collectExtendedMetrics }
-            }
-            if (collectExtendedMetrics === "boolean" || !collectExtendedMetrics) {
-                options.extendedMetrics = {
-                    [ExtendedMetricType.gc]: value,
-                    [ExtendedMetricType.heap]: value,
-                    [ExtendedMetricType.loop]: value,
-                }
-            }
+export function setAutoCollectPerformance(options: ApplicationInsightsOptions, value: boolean, collectExtendedMetrics?: any) {
+    if (options) {
+        options.enableAutoCollectPerformance = value;
+        if (typeof collectExtendedMetrics === "object") {
+            options.extendedMetrics = { ...collectExtendedMetrics }
         }
-    }
-
-    static setAutoCollectRequests(options: ApplicationInsightsOptions, value: boolean) {
-        if (options) {
-            if (value === false) {
-                options.instrumentationOptions = {
-                    http: {
-                        ...options.instrumentationOptions?.http,
-                        enabled: true,
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        ignoreIncomingRequestHook: (request: http.RequestOptions) => true,
-                    } as HttpInstrumentationConfig
-                };
-            } else {
-                options.instrumentationOptions = {
-                    http: {
-                        ...options.instrumentationOptions?.http,
-                        enabled: true,
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        ignoreIncomingRequestHook: (request: http.RequestOptions) => false,
-                    } as HttpInstrumentationConfig
-                };
-            }
-        }
-    }
-
-    static setAutoCollectDependencies(options: ApplicationInsightsOptions, value: boolean) {
-        if (options) {
-            if (value === false) {
-                options.instrumentationOptions = {
-                    http: {
-                        ...options.instrumentationOptions?.http,
-                        enabled: true,
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        ignoreOutgoingRequestHook: (request: http.RequestOptions) => true,
-                    } as HttpInstrumentationConfig
-                };
-            } else {
-                options.instrumentationOptions = {
-                    http: {
-                        ...options.instrumentationOptions?.http,
-                        enabled: true,
-                        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-                        ignoreOutgoingRequestHook: (request: http.RequestOptions) => false,
-                    } as HttpInstrumentationConfig
-                };
-            }
-        }
-    }
-
-    static setAutoCollectConsole(options: ApplicationInsightsOptions, value: boolean, enableConsole: boolean, collectConsoleLog = false) {
-        if (options) {
-            options.logInstrumentations = {
-                bunyan: { enabled: value },
-                winston: { enabled: value },
-                console: { enabled: collectConsoleLog },
+        if (collectExtendedMetrics === "boolean" || !collectExtendedMetrics) {
+            options.extendedMetrics = {
+                [ExtendedMetricType.gc]: value,
+                [ExtendedMetricType.heap]: value,
+                [ExtendedMetricType.loop]: value,
             }
         }
     }
 }
 
-export = ConfigHelper;
+export function setAutoCollectRequests(options: ApplicationInsightsOptions, value: boolean) {
+    if (options) {
+        if (value === false) {
+            options.instrumentationOptions = {
+                http: {
+                    ...options.instrumentationOptions?.http,
+                    enabled: true,
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    ignoreIncomingRequestHook: (request: http.RequestOptions) => true,
+                } as HttpInstrumentationConfig
+            };
+        } else {
+            options.instrumentationOptions = {
+                http: {
+                    ...options.instrumentationOptions?.http,
+                    enabled: true,
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    ignoreIncomingRequestHook: (request: http.RequestOptions) => false,
+                } as HttpInstrumentationConfig
+            };
+        }
+    }
+}
+
+export function setAutoCollectDependencies(options: ApplicationInsightsOptions, value: boolean) {
+    if (options) {
+        if (value === false) {
+            options.instrumentationOptions = {
+                http: {
+                    ...options.instrumentationOptions?.http,
+                    enabled: true,
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    ignoreOutgoingRequestHook: (request: http.RequestOptions) => true,
+                } as HttpInstrumentationConfig
+            };
+        } else {
+            options.instrumentationOptions = {
+                http: {
+                    ...options.instrumentationOptions?.http,
+                    enabled: true,
+                    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+                    ignoreOutgoingRequestHook: (request: http.RequestOptions) => false,
+                } as HttpInstrumentationConfig
+            };
+        }
+    }
+}
+
+export function setAutoCollectConsole(options: ApplicationInsightsOptions, value: boolean, enableConsole: boolean, collectConsoleLog = false) {
+    if (options) {
+        options.logInstrumentations = {
+            bunyan: { enabled: value },
+            winston: { enabled: value },
+            console: { enabled: collectConsoleLog },
+        }
+    }
+}
