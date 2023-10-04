14a01ad213ab9de650de23c88170375f87a4631f
Jackson Weber <47067795+JacksonWeber@users.noreply.github.com>
2023-10-03 23:41:15 -0700

    Add functions writer tests.

---
+++ test/unitTests/agent/diagnostics/writers/azureFunctionsWriter.tests.ts
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import * as assert from "assert";
+import * as sinon from "sinon";
+import { AzureFunctionsWriter } from "../../../../../src/agent/diagnostics/writers/azureFunctionsWriter";
+
+describe("agent//diagnostics/writers/fileWriter", () => {
+    let sandbox: sinon.SinonSandbox;
+    before(() => {
+        sandbox = sinon.createSandbox();
+    });
+
+    afterEach(() => {
+        sandbox.restore();
+    });
+
+    it("should log", () => {
+        const writer = new AzureFunctionsWriter("testIkey");
+        const consoleInfoStub = sandbox.stub(console, "info");
+        writer.log("test");
+        assert.ok(consoleInfoStub.calledOnce);
+    });
+
+    it("should error", () => {
+        const writer = new AzureFunctionsWriter("testIkey");
+        const consoleErrorStub = sandbox.stub(console, "error");
+        writer.error("test error");
+        assert.ok(consoleErrorStub.calledOnce);
+    });
+});
