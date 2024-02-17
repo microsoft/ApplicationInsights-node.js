ca8330735b4913b2eba0f8ff615df9e80da16065
Jackson Weber <jacksonweber@microsoft.com>
2024-02-16 18:16:51 -0800

    Fix auto exception collection.

--- src/logs/exceptions.ts
+++ src/logs/exceptions.ts
@@ -6,86 +6,57 @@ import { LogApi } from "../shim/logsApi";
 import { LoggerProvider } from "@opentelemetry/sdk-logs";
 
 type ExceptionHandle = "uncaughtExceptionMonitor" | "uncaughtException" | "unhandledRejection";
-const UNCAUGHT_EXCEPTION_MONITOR_HANDLER_NAME: ExceptionHandle = "uncaughtExceptionMonitor";
 const UNCAUGHT_EXCEPTION_HANDLER_NAME: ExceptionHandle = "uncaughtException";
 const UNHANDLED_REJECTION_HANDLER_NAME: ExceptionHandle = "unhandledRejection";
 const FALLBACK_ERROR_MESSAGE =
     "A promise was rejected without providing an error. Application Insights generated this error stack for you.";
 
 export class AutoCollectExceptions {
-    private _canUseUncaughtExceptionMonitor = false;
     private _exceptionListenerHandle?: (error: Error | undefined) => void;
     private _rejectionListenerHandle?: (error: Error | undefined) => void;
     private _client: LogApi;
 
     constructor(client: LogApi) {
         this._client = client;
-        // Only use for 13.7.0+
-        const nodeVer = process.versions.node.split(".");
-        this._canUseUncaughtExceptionMonitor =
-            parseInt(nodeVer[0]) > 13 || (parseInt(nodeVer[0]) === 13 && parseInt(nodeVer[1]) >= 7);
-
-        // For scenarios like Promise.reject(), an error won't be passed to the handle. Create a placeholder
-        // error for these scenarios.
-        if (this._canUseUncaughtExceptionMonitor) {
-            // Node.js >= 13.7.0, use uncaughtExceptionMonitor. It handles both promises and exceptions
-            this._exceptionListenerHandle = this._handleException.bind(
-                this,
-                false,
-                UNCAUGHT_EXCEPTION_MONITOR_HANDLER_NAME
-            ); // never rethrows
-            (<any>process).on(
-                UNCAUGHT_EXCEPTION_MONITOR_HANDLER_NAME,
-                this._exceptionListenerHandle
-            );
-        } else {
-            this._exceptionListenerHandle = this._handleException.bind(
-                this,
-                true,
-                UNCAUGHT_EXCEPTION_HANDLER_NAME
-            );
-            this._rejectionListenerHandle = this._handleException.bind(
-                this,
-                false,
-                UNHANDLED_REJECTION_HANDLER_NAME
-            ); // never rethrows
-            (<any>process).on(
-                UNCAUGHT_EXCEPTION_HANDLER_NAME,
-                this._exceptionListenerHandle
-            );
-            (<any>process).on(
-                UNHANDLED_REJECTION_HANDLER_NAME,
-                this._rejectionListenerHandle
-            );
-        }
+        this._exceptionListenerHandle = this._handleException.bind(
+            this,
+            true,
+            UNCAUGHT_EXCEPTION_HANDLER_NAME
+        );
+        this._rejectionListenerHandle = this._handleException.bind(
+            this,
+            false,
+            UNHANDLED_REJECTION_HANDLER_NAME
+        ); // never rethrows
+        (<any>process).on(
+            UNCAUGHT_EXCEPTION_HANDLER_NAME,
+            this._exceptionListenerHandle
+        );
+        (<any>process).on(
+            UNHANDLED_REJECTION_HANDLER_NAME,
+            this._rejectionListenerHandle
+        );
     }
 
     public shutdown() {
         if (this._exceptionListenerHandle) {
-            if (this._canUseUncaughtExceptionMonitor) {
+            if (this._exceptionListenerHandle) {
                 process.removeListener(
-                    UNCAUGHT_EXCEPTION_MONITOR_HANDLER_NAME,
+                    UNCAUGHT_EXCEPTION_HANDLER_NAME,
                     this._exceptionListenerHandle
                 );
-            } else {
-                if (this._exceptionListenerHandle) {
-                    process.removeListener(
-                        UNCAUGHT_EXCEPTION_HANDLER_NAME,
-                        this._exceptionListenerHandle
-                    );
-                }
-                if (this._rejectionListenerHandle) {
-                    process.removeListener(
-                        UNHANDLED_REJECTION_HANDLER_NAME,
-                        this._rejectionListenerHandle
-                    );
-                }
             }
-            this._exceptionListenerHandle = undefined;
-            this._rejectionListenerHandle = undefined;
-            delete this._exceptionListenerHandle;
-            delete this._rejectionListenerHandle;
+            if (this._rejectionListenerHandle) {
+                process.removeListener(
+                    UNHANDLED_REJECTION_HANDLER_NAME,
+                    this._rejectionListenerHandle
+                );
+            }
         }
+        this._exceptionListenerHandle = undefined;
+        this._rejectionListenerHandle = undefined;
+        delete this._exceptionListenerHandle;
+        delete this._rejectionListenerHandle;
     }
 
     private _handleException(
@@ -95,14 +66,15 @@ export class AutoCollectExceptions {
     ) {
         if (this._client) {
             this._client.trackException({ exception: error });
-            (logs.getLoggerProvider() as LoggerProvider).forceFlush();
-            // only rethrow when we are the only listener
-            if (reThrow && name && process.listeners(name as any).length === 1) {
-                // eslint-disable-next-line no-console
-                console.error(error);
-                // eslint-disable-next-line no-process-exit
-                process.exit(1);
-            }
+            (logs.getLoggerProvider() as LoggerProvider).forceFlush().then(() => {
+                // only rethrow when we are the only listener
+                if (reThrow && name && process.listeners(name as any).length === 1) {
+                    // eslint-disable-next-line no-console
+                    console.error(error);
+                    // eslint-disable-next-line no-process-exit
+                    process.exit(1);
+                }
+            });
         } else {
             // eslint-disable-next-line no-console
             console.error(error);
