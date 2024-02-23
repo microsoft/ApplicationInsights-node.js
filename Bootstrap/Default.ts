b62bff09e9e98db737b0ce3530740715662979f9
Jackson Weber <jacksonweber@microsoft.com>
2024-02-23 10:18:26 -0800

    Update Default.ts

--- Bootstrap/Default.ts
+++ Bootstrap/Default.ts
@@ -15,7 +15,6 @@ let _appInsights: typeof types | null;
 
 export const defaultConfig = new Config(); // Will read env variables, expose for Agent initialization
 let _prefix = `${PrefixHelpers.getResourceProvider()}${PrefixHelpers.getOsPrefix()}${Constants.AttachTypePrefix.INTEGRATED_AUTO}_`;
-Context.sdkPrefix = _prefix;
 let _fullSdkVersion = `${_prefix}node:${Context.sdkVersion}`;
 const _instrumentationKey = defaultConfig.instrumentationKey;
 let _logger: DiagnosticLogger = new DiagnosticLogger(console, _instrumentationKey);
@@ -112,6 +111,7 @@ export function setupAndStart(aadTokenCredential?: azureCoreAuth.TokenCredential
         }
 
         // Instrument the SDK
+        Context.sdkPrefix = _prefix;
         _appInsights.setup();
         const appInsightsSDKVersion = _appInsights.defaultClient.context.keys.internalSdkVersion;
 
