d8e0aedcc0e97b3fff324e620efa169846926784
Jackson Weber <jacksonweber@microsoft.com>
2024-04-16 11:00:44 -0700

    Update workflows and readme.

--- src/types.ts
+++ src/types.ts
@@ -7,7 +7,7 @@ import { InstrumentationConfig } from "@opentelemetry/instrumentation";
 import { OTLPExporterNodeConfigBase } from "@opentelemetry/otlp-exporter-base";
 
 
-export const AZURE_MONITOR_OPENTELEMETRY_VERSION = "1.0.0-beta.12";
+export const AZURE_MONITOR_OPENTELEMETRY_VERSION = "1.3.0";
 export const DEFAULT_ROLE_NAME = "Web";
 process.env["AZURE_MONITOR_DISTRO_VERSION"] = AZURE_MONITOR_OPENTELEMETRY_VERSION;
 
