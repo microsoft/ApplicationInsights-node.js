5fc305012a4c59c4a77436b997eb9b0174a3bceb
Hector Hernandez <39923391+hectorhdzg@users.noreply.github.com>
2024-06-27 16:26:41 -0700

    Adding ETW Diagnostic Logger for Windows Agent (#1350)
    * Adding ETW Diagnostic Logger por Windows Agent
    
    * Add lint exceptions

---
+++ src/agent/diagnostics/etwDiagnosticLogger.ts
@@ -0,0 +1,27 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT license.
+
+import { IDiagnosticLog } from "../types";
+import { BaseDiagnosticLogger } from "./baseDiagnosticLogger";
+import { EtwWritter } from "./writers/etwWritter";
+
+
+export class EtwDiagnosticLogger extends BaseDiagnosticLogger {
+
+    constructor(instrumentationKey: string) {
+        super(instrumentationKey);
+        this._agentLogger = new EtwWritter();
+    }
+
+    public logMessage(diagnosticLog: IDiagnosticLog) {
+        const metaData = this._getMetadata();
+        metaData.push(diagnosticLog.messageId || "");
+        const message: string = diagnosticLog.message;
+        this._agentLogger.log(message, metaData);
+    }
+
+    private _getMetadata(): [string, string, string, string, string] {
+        // Must return strings in this exact order!
+        return [ this._extensionVersion, this._subscriptionId, this._siteName, this._sdkVersion, this._instrumentationKey];
+      }
+}
