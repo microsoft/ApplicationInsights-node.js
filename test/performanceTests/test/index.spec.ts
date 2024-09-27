74455b7c1f4e9870b92014ed2b1db3b5e2a30640
Jackson Weber <jacksonweber@microsoft.com>
2024-09-27 14:40:38 -0700

    Put test results in a file for parsing by pipeline.

--- test/performanceTests/test/index.spec.ts
+++ test/performanceTests/test/index.spec.ts
@@ -4,6 +4,15 @@
 import { createPerfProgram } from "@azure-tools/test-perf";
 import { TrackDependencyTest } from "./trackDependency.spec.js";
 import { TrackTraceTest } from "./trackTrace.spec.js";
+import fs from "fs";
+import util from "util";
 
+// Write console logs to file for parsing
+const logFile = fs.createWriteStream("perf.log", { flags: "a" });
+const logStdout = process.stdout;
+console.log = function() {
+    logFile.write(util.format.apply(null, arguments) + '\n');
+    logStdout.write(util.format.apply(null, arguments) + '\n');
+};
 const perfProgram = createPerfProgram(TrackDependencyTest, TrackTraceTest);
 perfProgram.run();
