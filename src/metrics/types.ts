2e187ef2e44a85bbd3ba6b99020a4a00403df9e2
Jackson Weber <47067795+JacksonWeber@users.noreply.github.com>
2025-04-22 15:38:19 -0700

    Release 3.7.0 (#1424)
    * Release 3.7.0
    
    * Fix test.
    
    * Update README.md
    
    * Update README.md

--- src/metrics/types.ts
+++ src/metrics/types.ts
@@ -5,38 +5,3 @@
  * Disable Standard Metrics environment variable name.
  */
 export const APPLICATION_INSIGHTS_NO_STANDARD_METRICS = "APPLICATION_INSIGHTS_NO_STANDARD_METRICS";
-
-export interface StandardMetricBaseDimensions {
-  metricId?: string;
-  cloudRoleInstance?: string;
-  cloudRoleName?: string;
-  IsAutocollected?: string;
-}
-
-export interface MetricRequestDimensions extends StandardMetricBaseDimensions {
-  requestSuccess?: string;
-  requestResultCode?: string;
-}
-
-export interface MetricDependencyDimensions extends StandardMetricBaseDimensions {
-  dependencyType?: string;
-  dependencyTarget?: string;
-  dependencySuccess?: string;
-  dependencyResultCode?: string;
-}
-
-export enum StandardMetricNames {
-  HTTP_REQUEST_DURATION = "azureMonitor.http.requestDuration",
-  HTTP_DEPENDENCY_DURATION = "azureMonitor.http.dependencyDuration",
-  EXCEPTION_COUNT = "azureMonitor.exceptionCount",
-  TRACE_COUNT = "azureMonitor.traceCount",
-}
-
-export enum PerformanceCounterMetricNames {
-  PRIVATE_BYTES = "Private_Bytes",
-  AVAILABLE_BYTES = "Available_Bytes",
-  PROCESSOR_TIME = "Processor_Time",
-  PROCESS_TIME = "Process_Time",
-  REQUEST_RATE = "Request_Rate",
-  REQUEST_DURATION = "Request_Execution_Time",
-}
